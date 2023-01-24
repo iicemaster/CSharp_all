0b34e5a116e5063013f8dfa2a9afaea7e0a69457
iicemaster <iicemaster@yandex.ru>
2023-01-24 11:17:46 +0300

    Первое задание готово

--- Seminar2/homework1/Program.cs
+++ Seminar2/homework1/Program.cs
@@ -5,7 +5,40 @@
 
 Console.WriteLine("Вариант с обработкой данных от пользователя");
 Console.WriteLine("---------");
-Console.Write("Введите число: ");
-string A_num = Console.ReadLine;
-int A = Convert.ToInt32(A_num);
+Console.Write("Введите число: "); // есть вариант с реализацией большего количества символов
+string A_num = Console.ReadLine();
+// int A = Convert.ToInt32(A_num);  - Конвертация не обязательна т.к. не происходит никаких вычислений.
+int L = A_num.Length;
+int i;
+for (i = 0; i < L; i++)
+{
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num[i]}");}
+}
+Console.WriteLine($"Всего символов: {L}");
 
+Console.WriteLine("---------");
+Console.WriteLine("Задание №1");
+string A_num1 = "456";
+int L1 = A_num1.Length;
+for (i = 0; i < L1; i++)
+{
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num1[i]}");}
+}
+
+Console.WriteLine("---------");
+Console.WriteLine("Задание №2");
+string A_num2 = "782";
+int L2 = A_num2.Length;
+for (i=0; i<L2; i++)
+{
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num2[i]}");}
+}
+
+Console.WriteLine("---------");
+Console.WriteLine("Задание №3");
+string A_num3 = "918";
+int L3 = A_num3.Length;
+for (i=0; i<L3; i++)
+{
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num3[i]}");}
+}
