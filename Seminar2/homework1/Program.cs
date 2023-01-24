9f42ff61a68b2280cd5090ec059a7ee8ceaf6b5f
iicemaster <iicemaster@yandex.ru>
2023-01-24 11:37:37 +0300

    Добавил 2 задание

--- Seminar2/homework1/Program.cs
+++ Seminar2/homework1/Program.cs
@@ -5,7 +5,7 @@
 
 Console.WriteLine("Вариант с обработкой данных от пользователя");
 Console.WriteLine("---------");
-Console.Write("Введите число: "); // есть вариант с реализацией большего количества символов
+Console.Write("Введите число: "); 
 string A_num = Console.ReadLine();
 // int A = Convert.ToInt32(A_num);  - Конвертация не обязательна т.к. не происходит никаких вычислений.
 int L = A_num.Length;
@@ -22,7 +22,7 @@ string A_num1 = "456";
 int L1 = A_num1.Length;
 for (i = 0; i < L1; i++)
 {
-    if (i == 1) {Console.WriteLine($"Второй символ: {A_num1[i]}");}
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num1[i]}. Всего символов: {L1}");}
 }
 
 Console.WriteLine("---------");
@@ -31,7 +31,7 @@ string A_num2 = "782";
 int L2 = A_num2.Length;
 for (i=0; i<L2; i++)
 {
-    if (i == 1) {Console.WriteLine($"Второй символ: {A_num2[i]}");}
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num2[i]}. Всего символов: {L2}");}
 }
 
 Console.WriteLine("---------");
@@ -40,5 +40,5 @@ string A_num3 = "918";
 int L3 = A_num3.Length;
 for (i=0; i<L3; i++)
 {
-    if (i == 1) {Console.WriteLine($"Второй символ: {A_num3[i]}");}
+    if (i == 1) {Console.WriteLine($"Второй символ: {A_num3[i]}. Всего символов: {L3}");}
 }
--- Seminar2/homework2/Program.cs
+++ Seminar2/homework2/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.WriteLine("Вариант с обработкой данных от пользователя");
+Console.WriteLine("---------");
+Console.Write("Введите число: "); 
+string A_num = Console.ReadLine();
+// int A = Convert.ToInt32(A_num);  - Конвертация не обязательна т.к. не происходит никаких вычислений.
+int L = A_num.Length;
+int i;
+if (L>=3)
+{
+   for (i = 0; i < L; i++)
+   {
+       if (i == 2) {Console.WriteLine($"Третий символ: {A_num[i]}. Всего символов: {L}");}
+   }
+}
+else {Console.WriteLine($"Третьей цифры нет. Всего символов: {L}");}
+
+Console.WriteLine("---------");
+Console.WriteLine("Задание №1");
+string A_num1 = "645";
+int L1 = A_num1.Length;
+if (L1>=3)
+{
+    for (i=0; i < L1; i++)
+    {
+        if (i == 2) {Console.WriteLine($"Третий символ: {A_num1[i]}. Всего символов: {L1}");}
+    }
+}
+else {Console.WriteLine($"Третьей цифры нет. Всего символов: {L1}");}
+
+Console.WriteLine("---------");
+Console.WriteLine("Задание №2");
+string A_num2 = "78";
+int L2 = A_num2.Length;
+if (L2>=3)
+{
+    for (i=0; i < L2; i++)
+    {
+        if (i == 2) {Console.WriteLine($"Третий символ: {A_num1[i]}. Всего символов: {L2}");}
+    }
+}
+else {Console.WriteLine($"Третьей цифры нет. Всего символов: {L2}");}
+
+Console.WriteLine("---------");
+Console.WriteLine("Задание №3");
+string A_num3 = "32679";
+int L3 = A_num3.Length;
+if (L3>=3)
+{
+    for (i=0; i < L3; i++)
+    {
+        if (i == 2) {Console.WriteLine($"Третий символ: {A_num3[i]}. Всего символов: {L3}");}
+    }
+}
+else {Console.WriteLine($"Третьей цифры нет. Всего символов: {L3}");}
