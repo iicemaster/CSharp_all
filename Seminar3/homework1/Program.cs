a0a9ec2ccfa041dd09ed0d2c4f0b91907e039266
iicemaster <iicemaster@yandex.ru>
2023-01-27 01:26:17 +0300

    Вносим правки

--- Seminar3/homework1/Program.cs
+++ Seminar3/homework1/Program.cs
@@ -15,21 +15,19 @@ string n_reverse;
 //string n_reverse;
 char [] charStr = text.ToCharArray();
 for (i = 0; i<=N-1; i++, n--)
-     {
-         charStr[n] = text[i];
-         //if (i == 2) {Console.WriteLine($"Третий символ: {A_num[i]}. Всего символов: {L}");} 
-     }
+     {charStr[n] = text[i];}
  n_reverse = new string(charStr);
  if (text == n_reverse){Console.WriteLine("Является палиндромом");}
  else {Console.WriteLine("Не является палиндромом");}
 
-// ВАРИАНТ С ИСПОЛЬЗОВАНИЕМ МЕТОДА a.Reverse();
-
-Console.WriteLine("Вариант с вводом данных через консоль");
+// ВАРИАНТ С ИСПОЛЬЗОВАНИЕМ МЕТОДА Reverse();
 Console.WriteLine("-------");
+Console.WriteLine("Второй вариант, более короткий и без цикла.");
 Console.Write("Введите число:  ");
-string text = Console.ReadLine();
-int N = text.Length;
-int n = N-1;
-int i;
-string n_reverse;
+string chartext22;
+string text2 = Console.ReadLine();
+char[] chartext2 = text2.ToCharArray();
+Array.Reverse(chartext2);
+chartext22 = new string(chartext2);
+if (text2 == chartext22){Console.WriteLine("Является палиндромом");}
+else {Console.WriteLine("Не является палиндромом");}
