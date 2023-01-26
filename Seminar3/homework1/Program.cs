a0b8b615220437e4ddd0616766e673fdf74c0c91
iicemaster <iicemaster@yandex.ru>
2023-01-27 01:03:05 +0300

    Решил 1 задачу

--- Seminar3/homework1/Program.cs
+++ Seminar3/homework1/Program.cs
@@ -4,3 +4,32 @@
 12821 -> да
 23432 -> да */
 
+Console.WriteLine("Вариант с вводом данных через консоль");
+Console.WriteLine("-------");
+Console.Write("Введите число:  ");
+string text = Console.ReadLine();
+int N = text.Length;
+int n = N-1;
+int i;
+string n_reverse;
+//string n_reverse;
+char [] charStr = text.ToCharArray();
+for (i = 0; i<=N-1; i++, n--)
+     {
+         charStr[n] = text[i];
+         //if (i == 2) {Console.WriteLine($"Третий символ: {A_num[i]}. Всего символов: {L}");} 
+     }
+ n_reverse = new string(charStr);
+ if (text == n_reverse){Console.WriteLine("Является палиндромом");}
+ else {Console.WriteLine("Не является палиндромом");}
+
+// ВАРИАНТ С ИСПОЛЬЗОВАНИЕМ МЕТОДА a.Reverse();
+
+Console.WriteLine("Вариант с вводом данных через консоль");
+Console.WriteLine("-------");
+Console.Write("Введите число:  ");
+string text = Console.ReadLine();
+int N = text.Length;
+int n = N-1;
+int i;
+string n_reverse;
