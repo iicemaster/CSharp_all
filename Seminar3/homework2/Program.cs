5b1ad5af0218ac18ddad4df0731382521f91ed4b
iicemaster <iicemaster@yandex.ru>
2023-01-26 23:36:40 +0300

    Выполнил две задачи

--- Seminar3/homework2/Program.cs
+++ Seminar3/homework2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+Console.WriteLine("---------");
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("Введите Х: "); 
+int X_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: "); 
+int Y_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z: "); 
+int Z_num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("Введите Х: "); 
+int XX_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: "); 
+int YY_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z: "); 
+int ZZ_num = Convert.ToInt32(Console.ReadLine());
+double Method1(int num1, int num2)
+{
+double res_m1 = Math.Pow((num1-num2),2);
+return res_m1;
+}
+double result = (Math.Pow(Method1(X_num, XX_num) + Method1(Y_num, YY_num)+ Method1(Z_num, ZZ_num), 0.5));
+Console.WriteLine($"Расстояние между точками: {result.ToString("0.00")}");
--- Seminar3/homework3/Program.cs
+++ Seminar3/homework3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+
+Console.WriteLine("Вариант с вводом данных через консоль");
+Console.WriteLine("-------");
+Console.Write("Введите число:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i;
+for (i=1; i <= N; i++) {
+Console.Write($"{Math.Pow(i, 2)} ");}
