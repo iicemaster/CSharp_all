18d084bab6e0dd97029ba08dde4e2517222649aa
iicemaster <iicemaster@yandex.ru>
2023-01-31 15:06:26 +0300

    Решил вторую задачу

--- Seminar4/Homework2/Program.cs
+++ Seminar4/Homework2/Program.cs
@@ -2,3 +2,13 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Вариант с вводом данных через консоль");
+Console.WriteLine("-------");
+string text = Console.ReadLine();
+int i, temp;
+for (i=0, temp=0; i<=text.Length-1; i++)
+     {
+        temp = temp + (int.Parse(text[i].ToString()));
+     } // Пузырьковым методом по 1 символу переносим данные
+Console.WriteLine($"Результат: {temp}");
