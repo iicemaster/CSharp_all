c13be63e18cc3e8a0a51780e1fd7788ae6ae0aac
iicemaster <iicemaster@yandex.ru>
2023-02-28 18:44:57 +0300

    Добавили 3 задачу

--- Seminar8/Homework3/Program.cs
+++ Seminar8/Homework3/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+//15 18
+
+int ConsoleImport(string input)
+{
+    Console.Write(input);
+    bool result;
+    int x;
+    do
+    {
+        result = int.TryParse((Console.ReadLine()), out x);
+        if (result == false) { Console.WriteLine("Вы ввели не число или число меньше 1. Попробуйте еще раз"); }
+        else { break; }
+    }
+    while (result == false);
+    return x;
+}
+
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine();
+int rows = ConsoleImport("Введите количество строк: ");
+int columns = ConsoleImport("Введите количество столбцов: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+FillArrayRandom(array);
+PrintArray2D(array);
+
+Console.WriteLine();
+
+FillArrayRandom(secondArray);
+PrintArray2D(secondArray);
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+Console.WriteLine("\nРезультат произоведения:\n");
+PrintArray2D(resultArray);
