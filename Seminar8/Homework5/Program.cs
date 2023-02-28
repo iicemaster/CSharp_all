4e8994152cfeba5971501739ef2bd67af2913453
iicemaster <iicemaster@yandex.ru>
2023-02-28 19:01:50 +0300

    Добавили задачу, внесли правки

--- Seminar8/Homework5/Program.cs
+++ Seminar8/Homework5/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int ConsoleImport(string input)
+{
+    Console.Write(input);
+    bool result;
+    int x;
+    do
+    {
+        result = int.TryParse((Console.ReadLine()), out x);
+        if (result == false) { Console.WriteLine("Вы ввели не число или число меньше 1. Попробуйте еще раз"); }
+        else { break; }
+    }
+    while (result == false);
+    return x;
+}
+
+int length = ConsoleImport("Введите размерность: ");
+int[,] array = new int[length, length];
+int number = 1;
+int i = 0;
+int j = 0;
+while (number <= length * length)
+{
+    array[i, j] = number;
+    if (i <= j + 1 && i + j < length - 1)
+        j++;
+    else if (i < j && i + j >= length - 1)
+        i++;
+    else if (i >= j && i + j > length - 1)
+        j--;
+    else
+        i--;
+    number++;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
