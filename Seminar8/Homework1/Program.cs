9cb82677995d8f32d541b03a1efa98573ec12a8b
iicemaster <iicemaster@yandex.ru>
2023-02-28 18:25:37 +0300

    Решили 2 задачу

--- Seminar8/Homework1/Program.cs
+++ Seminar8/Homework1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+// Метод получения данных из консоли формата int32
+int ConsoleImport()
+{
+    bool result;
+    int x;
+    do
+    {
+        result = int.TryParse((Console.ReadLine()), out x);
+        if (result == false) { Console.WriteLine("Вы ввели не число или число меньше 1. Попробуйте еще раз"); }
+        else { break; }
+    }
+    while (result == false);
+    return x;
+}
+
+// Метод создания двумерной матрицы
+int[,] CreateMatrix(int x, int y)
+{
+    int[,] matrix = new int[x, y];
+    return matrix;
+}
+// Метод заполнения двумерной матрицы
+//
+int[,] FillMatrix(int[,] matrix, int x, int y)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j<y; j++)
+        {
+            matrix [i,j] = rnd.Next(1, 10);
+            Console.Write($"{matrix[i,j]} ");
+            Thread.Sleep(500);
+        }
+        Console.WriteLine();
+        Thread.Sleep(500);
+    }
+return matrix;
+}
+
+// int SortMatrix()
+// {
+// }
+
+
+
+Console.Write("Введите количество столбцов(x): ");
+int x = ConsoleImport();
+Console.WriteLine();
+Console.Write("Введите количество строк(y): ");
+int y = ConsoleImport();
+Console.WriteLine();
+//Console.WriteLine($"Ваше число x: {x}, y: {y}");
+
+
+
+int[,] anal = FillMatrix(CreateMatrix(x, y), x, y);
+
+//Console.WriteLine(anal[7, 7]);
--- Seminar8/Homework2/Program.cs
+++ Seminar8/Homework2/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+
+int ConsoleImport(string input)
+{
+    Console.Write(input);
+    bool result;
+    int x;
+    do
+    {
+        result = int.TryParse((Console.ReadLine()), out x);
+        if (result == false) { Console.WriteLine("Вы ввели не число или число меньше 1. Попробуйте еще раз"); }
+        else { break; }
+    }
+    while (result == false);
+    return x;
+}
+
+
+Console.WriteLine("Введите размер массива m x n и диапазон случайных значений:");
+int m = ConsoleImport("Введите m: ");
+int n = ConsoleImport("Введите n: ");
+int range = ConsoleImport("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
