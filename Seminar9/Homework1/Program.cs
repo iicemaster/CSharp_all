b3fa0498b331ec6596e2db9f2010349988316d0f
iicemaster <iicemaster@yandex.ru>
2023-03-03 13:27:11 +0300

    Доделал 1 задание

--- Seminar9/Homework1/Program.cs
+++ Seminar9/Homework1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+// Тело задания
+int n = ConsoleImport("Введите число N(Программа выведет все натуральные числа в промужутке от N до 1): "); ;
+Console.WriteLine();
+NaturalNumbers(n);
+
+// Метод получения данных из консоли формата int32
+int ConsoleImport(string input)
+{
+    Console.Write(input);
+    bool result;
+    int x;
+    do
+    {
+        result = int.TryParse((Console.ReadLine()), out x);
+        if (result == false) { Console.WriteLine("Вы ввели не число или число меньше 1. Попробуйте еще раз"); }
+        else { break; }
+    }
+    while (result == false);
+    return x;
+}
+
+// Метод вывода натуральных чисел в заданном промежутке
+void NaturalNumbers(int number)
+{
+    if (number == 1) { Console.Write($"{number}"); }
+    else { Console.Write($"{number}, "); }
+    number = number - 1;
+    if (number >= 1)
+    {
+        NaturalNumbers(number);
+    }
+}
