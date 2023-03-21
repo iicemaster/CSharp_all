2cfcc9d851779111d5df9b9c1bfda78fbf270a6b
iicemaster <iicemaster@yandex.ru>
2023-03-21 20:28:21 +0300

    тест

---
+++ hctcpa/Program.cs
@@ -0,0 +1,156 @@
+﻿//project.Lists["hcaptcha"].Clear(); // Очистка списка зенки. Если все будет в кубике то она не нужна
+
+
+// Получаем Sitekey
+string text = project.ExecuteMacro(instance.ActiveTab.DomText);
+string sitekey ="";
+var errorIfEmpty = false;
+var regexGroups = Macros.TextProcessing.Regex(text, @"(?<=class=""h-captcha""\ data-sitekey="").*(?=""><iframe\ src="")", "0");
+if (regexGroups.Count == 0)
+{
+    if (errorIfEmpty) throw new Exception();
+}
+else
+{
+    sitekey = regexGroups[0].FirstOrDefault();
+}
+
+//Запись трафика
+instance.UseTrafficMonitoring = true;
+var traffic = instance.ActiveTab.GetTraffic();
+
+//клик по чекбоксу hcaptcha
+HtmlElement he = instance.ActiveTab.FindElementById("checkbox");
+if (he.IsVoid) return -1;
+
+instance.WaitFieldEmulationDelay();
+instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he);
+instance.ActiveTab.FullEmulationMouseClick("left", "click");
+Thread.Sleep(3000);
+
+string hcaptchabody = "";
+//получаем тело ответа от hcaptcha
+foreach(var t in traffic)
+    if (t.Url=="https://hcaptcha.com/getcaptcha/" + project.Variables["sitekey"].Value)
+        hcaptchabody = System.Text.Encoding.UTF8.GetString(t.ResponseBody);
+	
+return "Ничего не нашли";
+
+string hcaptchaQuest = "";
+// Получение текста задания
+var errorIfEmpty = true;
+var regexGroups = Macros.TextProcessing.Regex(hcaptchabody, @"(?<=""requester_question"":\{""en"":"").*(?=""},""requester_question_example"":)", "0");
+if (regexGroups.Count == 0)
+{
+    if (errorIfEmpty) throw new Exception();
+}
+else
+{
+    hcaptchaQuest = regexGroups[0].FirstOrDefault();
+}
+
+// Получаем изображения
+//string text = project.ExecuteMacro(project.Variables["hcaptchaBody"].Value);
+// Тут надо очистить список. Да и вообще было бы здорово всё в функцию загнать
+var hcaptcha_base64_images = new List<string>(); 
+var errorIfEmpty = false;
+var regexGroups = Macros.TextProcessing.Regex(hcaptchaBody, @"(?<=datapoint_uri"":"").*?(?="")");
+if (regexGroups.Count == 0)
+{
+    if (errorIfEmpty) throw new Exception();
+}
+else
+{
+    project.Lists["hcaptcha"].AddRange(regexGroups.Select(g => g.FirstOrDefault()));
+
+    for (int i =0; i<words.Length; i++ )    
+    {
+        hcaptcha_base64_images.Add(words[i]);
+    }
+
+}
+
+string[] words = Console.ReadLine().Replace(". ", " ").Replace(" .", " ").Replace(" ,", " ").Replace(", ", " ").Replace("  ", " ").Split(' ');
+//var hcaptcha_base64_images = new List<string>(); 
+// for (int i =0; i<words.Length; i++ ){
+// hcaptcha_base64_images.Add(words[i]);
+// }
+for(int i=0;i<hcaptcha_base64_images.Count;i++)
+{
+Console.WriteLine(hcaptcha_base64_images[i]);
+} 
+hcaptcha_base64_images.Clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Конвертируем ответ */
+string text = project.Variables["respArray"].Value;
+string[] array = text.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+List<int> hcatpchaClicks = new List<int>();
+
+foreach (var item in array.Select((value, i) => ( value, i )))
+	if(item.value.Equals("true"))
+		hcatpchaClicks.Add(item.i);
+
+/* Конвертируем ответ */
+
+/* */
+int maxCount = hcatpchaClicks.Count();
+instance.EmulationLevel = "Middle";
+bool page2 = false;
+
+HtmlElement he2 = instance.ActiveTab.FindElementByAttribute("div", "class", "button-submit\\ button", "regexp", 0);
+
+for(int i = 0; i < maxCount; i++)
+{
+	int m = hcatpchaClicks[i];
+	
+	HtmlElement he = instance.ActiveTab.FindElementByAttribute("div", "class", "border-focus", "regexp", m);
+	//he.RiseEvent("click", "Middle");
+	instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he);
+	instance.ActiveTab.FullEmulationMouseClick("left", "click");
+	
+	if (m > 8 && !page2)
+	{
+		//he2.RiseEvent("click", "None");
+		instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he2);
+	    instance.ActiveTab.FullEmulationMouseClick("left", "click");
+		HtmlElement he4 = instance.ActiveTab.FindElementByAttribute("div", "class", "border-focus", "regexp", m-9);
+		//he4.RiseEvent("click", "Middle");
+		instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he4);
+	    instance.ActiveTab.FullEmulationMouseClick("left", "click");
+		page2 = true;
+	}
+	else if (m > 8 && page2)
+	{
+		HtmlElement he3 = instance.ActiveTab.FindElementByAttribute("div", "class", "border-focus", "regexp", m-9);
+		//he3.RiseEvent("click", "Middle");
+		instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he3);
+	    instance.ActiveTab.FullEmulationMouseClick("left", "click");
+	}	
+}
+
+//he2.RiseEvent("click", "None");
+instance.ActiveTab.FullEmulationMouseMoveToHtmlElement(he2);
+instance.ActiveTab.FullEmulationMouseClick("click", "None");
---
+++ test_dict/Program.cs
@@ -0,0 +1,162 @@
+﻿// int count = 5;
+// for (int i=0; i<count; i++)
+//     Dictionary<string, string> dict = new Dictionary<string, string>
+//     {
+//         ["Hello"]= "Привет",
+//         ["How are you?"] = "Как дела?",
+//         ["Bye"] = "Пока",
+//     };
+
+// for (int i=0; i<dict.Count; i++)   
+//     {Console.WriteLine(dict[i]);}
+
+// var hcaptcha_base64_list = new List<string>();
+// string testim = "govno, mocha, parasha";
+// var result = Macros.TextProcessing.Regex(hcaptchaBody, @",");
+
+
+
+// hcaptcha_base64.Add("January");
+// hcaptcha_base64.Add("February");
+// hcaptcha_base64.Add("March");
+// hcaptcha_base64.Add("April");
+
+// for(int i=0;i<hcaptcha_base64.Count;i++)
+// {
+// Console.WriteLine(hcaptcha_base64[i]);
+// } 
+
+// string[] words = Console.ReadLine().Replace(". ", " ").Replace(" .", " ").Replace(" ,", " ").Replace(", ", " ").Replace("  ", " ").Split(' '); 
+// Console.WriteLine(words[2]);
+// // в конечном итоге можно вводить элементы в удобном для пользователя варианте. в т.ч. даже если разделители будут разные
+// // for (int i = 0; i<words.Length; i++)
+// // {if (Convert.ToInt32(words[i]) > 0) {count += 1;}}
+
+// Console.WriteLine(words[2]);
+// Array.Clear(words, 0, words.Length);
+// words = new string [];
+// Console.WriteLine(words[2]);
+
+string[] words = Console.ReadLine().Replace(". ", " ").Replace(" .", " ").Replace(" ,", " ").Replace(", ", " ").Replace("  ", " ").Split(' ');
+var hcaptcha_base64_images = new List<string>(); 
+for (int i =0; i<words.Length; i++ ){
+hcaptcha_base64_images.Add(words[i]);
+}
+for(int i=0;i<hcaptcha_base64_images.Count;i++)
+{
+Console.WriteLine(hcaptcha_base64_images[i]);
+} 
+hcaptcha_base64_images.Clear(); // очистка списка, проверено работает. 
+/*for(int i=0;i<hcaptcha_base64_images.Count;i++)
+{
+Console.WriteLine(hcaptcha_base64_images[i]);
+} */
+
+
+// for (int i=0; i<dict.Length; i++)   
+//     {Console.WriteLine(dict[i]);}
+// foreach (var Key in dict)
+//     Console.WriteLine(dict[Key.Value]);
+	//accessNumbers[phone.Value] = true;
+
+
+// // Допустим используем словарь для хранения номеров, но помним, 
+// // что ключ должен быть уникальным, иначе мы получим ошибку уникальных
+// // значений.
+// Dictionary<string, long> phones = new Dictionary<string, long> {
+// 	{ "Вася", 81112223344 },
+//   { "Петя", 82224445566 },
+//   { "Стёпа", 83335556677 }
+// };
+
+// // Допустим у нас есть метод отправки смс по номеру
+// void SendSMS(long phone, string msg)
+// {
+// 	// todo
+// }
+
+// // Допустим мы хотим стёпе отправить смс, получаем его номер по имени
+// SendSMS(phones["Стёпа"], "Привет, как дела?");
+// // Допустим мы получили ответное смс
+// var sms = GetSMS(phones["Стёпа"], "Привет, всё ок, а у тебя?");
+// // Воспользуемся методом, который позволит определить от кого смс и что пишет
+// Console.WriteLine($"{WhoSendSMS(sms.phone)} - {sms.msg}" );
+// Console.WriteLine();
+// sms = GetSMS(81111111111, "Здравствуйте вам одобрена...");
+// // Получим смс от кого-то ещё)
+// Console.WriteLine($"{WhoSendSMS(sms.phone)} - {sms.msg}");
+// Console.WriteLine();
+
+// // Допустим нам не понравился этот номер и мы не хотим больше от него получать смс
+// // создадим ещё один словарь, с помощью него будем блокировать номера телефонов
+// // и добавим туда наш новый номер с флагом false
+// Dictionary<long, bool> accessNumbers = new Dictionary<long, bool>
+// {
+// 	{ 81111111111, false }
+// };
+
+// // Также добавим туда все номера из нашей записной книжки с флагом true
+// foreach (var phone in phones)
+// 	accessNumbers[phone.Value] = true;
+// // Попробуем снова получить СМС
+// var newSms = GetSMSWithAccess(81111111111, "Здравствуйте вам одобрена...");
+// // Поскольку теперь нам может прийти null нужно проверить есть ли действительно 
+// // смс
+// if (newSms != null)
+// 	Console.WriteLine($"{WhoSendSMS(newSms.phone)} - {newSms.msg}");
+// else
+// 	Console.WriteLine($"Пришла СМС от заблокированного номера.");
+// Console.WriteLine();
+// // Ну и проверим сообщение от Васи например
+// newSms = GetSMSWithAccess(phones["Вася"], "Привет");
+// if (newSms != null)
+// 	Console.WriteLine($"{WhoSendSMS(newSms.phone)} - {newSms.msg}");
+// Console.WriteLine();
+// // Допустим хотим посмотреть все номера в нашей записной книжке
+// Console.WriteLine($"Всего номеров в записной книжке: {phones.Count}");
+// foreach (var phone in phones)
+// 	Console.WriteLine($"{phone.Key} - {phone.Value}");
+// // Допустим мы видим, что у нас записан Петя, с которым давно
+// // не общались и хотим удалить его номер
+// Console.WriteLine();
+// phones.Remove("Петя");
+// Console.WriteLine($"Всего номеров в записной книжке: {phones.Count}");
+// // Проверим снова телефонную книгу
+// foreach (var phone in phones)
+// 	Console.WriteLine($"{phone.Key} - {phone.Value}");
+// Console.WriteLine();
+// // Как насчёт информации о доступных номерах?
+// Console.WriteLine($"Всего записей в коллекции с доступными номерами: {accessNumbers.Count}");
+// foreach (var phone in accessNumbers)
+//     Console.WriteLine($"От номера {phone.Key} получать смс {(phone.Value ? "можно" : "нельзя")}.");
+
+// // Допустим есть метод, который возвращает нам текст сообщения и номер телефона
+// SMSInfo GetSMS(long phone, string msg)
+// {
+// 	return new SMSInfo { phone = phone, msg = msg };
+// }
+// SMSInfo? GetSMSWithAccess(long phone, string msg)
+// {
+// 	if (accessNumbers[phone])
+// 		return new SMSInfo { phone = phone, msg = msg };
+// 	return null;
+// }
+
+// // А теперь попробуем найти обратно, кому же принадлежит номер
+// string WhoSendSMS(long phone)
+// {
+// 	// Переберём все значения в словаре и найдём имя отправителя
+// 	foreach (var phoneInfo in phones)
+// 	{
+// 		if (phoneInfo.Value == phone)
+// 			return phoneInfo.Key;
+// 	}
+// 	return "Номер отсутствует в записной книжке!";
+// }
+
+// // Для следующего примера создадим специальный класс
+// class SMSInfo
+// {
+// 	public long phone { get; set; }
+// 	public string msg { get; set; }
+// }
